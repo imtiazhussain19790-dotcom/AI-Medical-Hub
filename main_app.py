926baf13f7442ad1ca4f5c21a3a637c7c941a339
imtiazhussain19790-dotcom <imtiazhussain19790@gmail.com>
2026-02-08 00:38:33 +0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -13,17 +13,17 @@ elif selection=="BMI Calculator":
   st.title("BMI Calculator")
   weight=st.number_input("Enter your Weight (kg)",min_value=0.1)
   height=st.number_input("Enter your Height (meters)",min_value=0.1)
-if height > 0:
+  if height > 0:
       bmi=weight / (height * height)
       st.write(f"### Your BMI is: {bmi:,2f}")
-if bmi < 18.5:
-      st.warning("Underweight")
-elif 18.5 <= bmi < 25:
-      st.success("Healthy")
-elif 25 <= bmi < 30:
-      st.info("Overweight")
-else:
-      st.error("Obesity")
+      if bmi < 18.5:
+st.warning("Underweight")
+      elif 18.5 <= bmi < 25:
+          st.success("Healthy")
+      elif 25 <= bmi < 30:
+          st.info("Overweight")
+      else:
+          st.error("Obesity")
 
 # اب ڈائیبیٹیز والا حصہ بالکل بائیں طرف (Margin پر) ہونا چاہیے
 elif selection == "Diabetes Prediction":
