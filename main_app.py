ab65e3b060a55673384989a5427a1e4abad392a4
imtiazhussain19790-dotcom <imtiazhussain19790@gmail.com>
2026-02-07 18:59:38 +0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -13,14 +13,14 @@ elif selection=="BMI Calculator":
   weight=st.number_input("Enter your Weight (kg)",min_value=0.1)
   height=st.number_input("Enter your Height (meters)",min_value=0.1)
   if height > 0:
-    bmi=weight / (height * height)
-    st.write(f"### Your BMI is: {bmi:,2f}")
-             if bmi < 18.5:
-                  st.warning("Underweight")
-             elif 18.5 <= bmi < 25:
-                  st.success("Healthy")
-             elif 25 <= bmi < 30:
-                  st.info("Overweight")
+      bmi=weight / (height * height)
+      st.write(f"### Your BMI is: {bmi:,2f}")
+      if bmi < 18.5:
+        st.warning("Underweight")
+      elif 18.5 <= bmi < 25:
+        st.success("Healthy")
+      elif 25 <= bmi < 30:
+        st.info("Overweight")
              else:
                   st.error("Obesity")
 # Placeholder for Diabetes App
